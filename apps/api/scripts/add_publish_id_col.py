ca601862b4459b24c47adc87a952ff41f36a2734
mateojkk <thesaintszn@gmail.com>
2026-05-15 13:22:26 +0100

    fix: use absolute path in migration script

--- apps/api/scripts/add_publish_id_col.py
+++ apps/api/scripts/add_publish_id_col.py
@@ -1,10 +1,11 @@
 import sys
 import os
 
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from server.database import engine, Base
-from sqlalchemy import text, inspect
+from sqlalchemy import inspect, text
+from server.database import engine
+
 
 def run():
     inspector = inspect(engine)
